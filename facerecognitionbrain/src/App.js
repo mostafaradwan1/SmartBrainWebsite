3f5a50e60546a092be4bba636c17c0b5781f7275
mostafaradwan1 <mostafaradwan1001@gmail.com>
2020-05-16 23:03:57 +0200

    refactor App to a class componet

--- facerecognitionbrain/src/App.js
+++ facerecognitionbrain/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Component } from 'react';
 import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
@@ -6,22 +7,23 @@ import './App.css';
 import Rank from './components/Rank/Rank';
 import Particles  from 'react-particles-js'
 import particles from './particles';
+import { render } from 'react-dom';
 
-
-
-
-function App() {
-  return (
-    <div className="App">
-      <Navigation/>
-      <Logo/>
-      <Particles className='particles' params={particles}/>
-      <Rank/>
-      <ImageLinkForm/>
-      {/* 
-      <FaceRecognition/> */}
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navigation/>
+        <Logo/>
+        <Particles className='particles' params={particles}/>
+        <Rank/>
+        <ImageLinkForm/>
+        {/* 
+        <FaceRecognition/> */}
+      </div>
+    );
+  }
 }
+
 
 export default App;
