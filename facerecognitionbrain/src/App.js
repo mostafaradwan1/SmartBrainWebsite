78ec0dafcb818a7acd957018c2f171a37709e0d2
mostafaradwan1 <mostafaradwan1001@gmail.com>
2020-05-17 02:32:06 +0200

    faceRacognition box , css

--- facerecognitionbrain/src/App.js
+++ facerecognitionbrain/src/App.js
@@ -18,9 +18,26 @@ class App extends Component {
     this.state={
       input:'',
       imageUrl:'',
+      box:{},
     }
   }
 
+  calculateFaceLocation=(data)=>{
+    const clarifaiFace=data.outputs[0].data.regions[0].region_info.bounding_box
+    const image= document.getElementById('input')
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox(box){
+    this.setState({box:box})
+  }
   onInputChange=(e)=>{
     this.setState({input:e.target.value})
   }
@@ -28,14 +45,8 @@ class App extends Component {
   onSubmit=()=>{
     this.setState({imageUrl:this.state.input})
 
-      app.models.predict( Clarifai.FACE_DETECT_MODEL,this.state.input).then(
-        function(response) {
-          // do something with response
-          console.log(response.outputs[0].data.regions[0].region_info.bounding_box)
-        },
-        function(err) {
-          // there was an error
-        }
+      app.models.predict( Clarifai.FACE_DETECT_MODEL,this.state.input).then(response => this.displayFaceBox(this.calculateFaceLocation(response))
+      .catch(err => console.log(err))
       );
   }
   render() {
@@ -46,7 +57,7 @@ class App extends Component {
         <Particles className='particles' params={particles}/>
         <Rank/>
         <ImageLinkForm onInputChange={this.onInputChange} onSubmit={this.onSubmit}/>
-        <FaceRecognition imageUrl={this.state.imageUrl}/> 
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/> 
       </div>
     );
   }
